f6bcb99d1da26c2babc2eac6ecc91d24aacf0b4f
Baptiste LECHAT <baptistelechat@outlook.fr>
2023-09-15 18:35:27 +0200

    🎨 Setup shadcn/ui for frontend

---
+++ app/src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "@ui/select";
+
+const App = () => {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default App;
